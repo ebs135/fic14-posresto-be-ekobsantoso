ca6cfa887a9aabf4bc1de91f88c743b55939ea59
Eko B Santoso <ebs135@gmail.com>
2024-02-11 23:08:40 +0700

    add api login-categories-products

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    // index api
+    public function index()
+    {
+        // get all products
+        $products = Product::all();
+
+        // get all products (pagination)
+        // $products = Product::paginate(10);
+
+        return response()->json([
+            'status' => 'success',
+            'data' => $products
+        ], 200);
+    }
+}
